31ae211b514bd3d1e118c33fac3d5f52ee10b74d
Matias Barandiaran <115642529+m4mbo@users.noreply.github.com>
2024-01-26 17:53:55 +0100

    Git ignore

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.ggj2024;
+import com.mygdx.game.Game.ggj2024;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
