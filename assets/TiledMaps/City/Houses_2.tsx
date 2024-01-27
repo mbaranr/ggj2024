559aca527b446ae595bcda5b11e7e5c2615d575f
kamepe <150451564+kamepe@users.noreply.github.com>
2024-01-27 18:09:21 +0100

    getting closer

---
+++ assets/TiledMaps/City/Houses_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Houses_2" tilewidth="16" tileheight="16" tilecount="961" columns="31">
+ <image source="houses.png" width="500" height="500"/>
+</tileset>
