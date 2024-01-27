19de3a2f914fde21a789858ee864f920266f673f
Matias Barandiaran <115642529+m4mbo@users.noreply.github.com>
2024-01-27 10:21:18 +0100

    King + item logic

--- core/src/com/mygdx/game/RoleCast/King.java
+++ core/src/com/mygdx/game/RoleCast/King.java
@@ -1,12 +1,60 @@
 package com.mygdx.game.RoleCast;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.Objects.Item;
 import com.mygdx.game.Sprites.B2Sprite;
+import com.mygdx.game.Tools.Constants;
+
+import java.util.LinkedList;
 
 public class King extends B2Sprite {
 
+
     private int laughMeter;
 
-    public King () {
+    public King(int x, int y, World world) {
+
+        laughMeter = 0;
+
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(x / Constants.PPM, y / Constants.PPM);
+        bdef.type = BodyDef.BodyType.StaticBody;
+        b2body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape polygonShape = new PolygonShape();
+
+        //Create body fixture
+        polygonShape.setAsBox(32 / Constants.PPM, 32 / Constants.PPM, new Vector2(0, 0), 0);
+        fdef.shape = polygonShape;
+        fdef.friction = 0;
+        fdef.filter.maskBits = Constants.BIT_GROUND | Constants.BIT_TREE | Constants.BIT_ITEM;
+        b2body.createFixture(fdef).setUserData("king");
+
+    }
+
+    public void loadSprites() {
+
+    }
+
+    public void handleAnimation() {
+
+    }
+
+    public void update(float delta) {
+
+        animation.update(delta);
+    }
+
+    public void resetMeter() {
+        laughMeter = 0;
+    }
+
+    public void presentItems(LinkedList<Item> items) {
 
     }
 
