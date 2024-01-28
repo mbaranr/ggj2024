39aa0b1c993dfd31b0c4644a4efc838667bb1c93
Matias Barandiaran <115642529+m4mbo@users.noreply.github.com>
2024-01-28 13:04:25 +0100

    Fixed label placing

--- core/src/com/mygdx/game/Scenes/HUD.java
+++ core/src/com/mygdx/game/Scenes/HUD.java
@@ -1,9 +1,12 @@
 package com.mygdx.game.Scenes;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Align;
@@ -22,6 +25,7 @@ public class HUD implements Subscriber {
     private int minutes;
     private Label timeLabel;
     private Label coinLabel;
+    private Image coinImage;
     private int coinCount;
     public HUD(MyTimer timer, SpriteBatch batch) {
         this.timer = timer;
@@ -36,17 +40,22 @@ public class HUD implements Subscriber {
         hour = 9;
         minutes = 0;
 
-        timeLabel = new Label(String.format("%02d %s", hour % 12 == 0 ? 12 : hour % 12, hour < 12 ? "AM" : "PM"), new Label.LabelStyle(FancyFontHelper.getInstance().getFont(Color.RED, 70), new Color(0.5f, 1, 0, 1)));
+        timeLabel = new Label(String.format("%02d%s", hour % 12 == 0 ? 12 : hour % 12, hour < 12 ? " AM" : " PM"), new Label.LabelStyle(FancyFontHelper.getInstance().getFont(Color.RED, 60), new Color(0.5f, 1, 0, 1)));
+        timeLabel.setAlignment(Align.left);
 
-        table.add(timeLabel).padTop(40);
+        coinLabel = new Label(Integer.toHexString(coinCount), new Label.LabelStyle(FancyFontHelper.getInstance().getFont(Color.YELLOW, 30), new Color(1, 1, 1, 1)));
+        coinLabel.setAlignment(Align.right);
+
+        coinImage = new Image(new Texture(Gdx.files.internal("Items/coin.png")));
+
+        table.add(timeLabel).padRight(550).padTop(40).expandX();
+        table.add(coinImage).width(40).height(60).padTop(40).padRight(10);
+        table.add(coinLabel).padRight(40).padTop(40);
         stage.addActor(table);
 
         coinCount = 0;
 
-        coinLabel = new Label("Coins: " + coinCount, new Label.LabelStyle(FancyFontHelper.getInstance().getFont(Color.YELLOW, 30), new Color(1, 1, 1, 1)));
-        coinLabel.setAlignment(Align.right);
-        coinLabel.setPosition(viewport.getWorldWidth() - 190, viewport.getWorldHeight() - 90);
-        stage.addActor(coinLabel);
+
     }
 
     public void start() {
@@ -64,7 +73,7 @@ public class HUD implements Subscriber {
         if (minutes == 60) {
             hour++;
         }
-        timeLabel.setText(String.format("%02d %s", hour % 12 == 0 ? 12 : hour % 12, hour < 12 ? "AM" : "PM"));
+        timeLabel.setText(String.format("%02d%s", hour % 12 == 0 ? 12 : hour % 12, hour < 12 ? " AM" : " PM"));
         timer.start(1f, "minute_passes", this);
     }
 
@@ -73,6 +82,6 @@ public class HUD implements Subscriber {
     }
     public void updateCoinCount(int newCoinCount) {
         coinCount = newCoinCount;
-        coinLabel.setText("Coin: " + coinCount);
+        coinLabel.setText(coinCount);
     }
 }
