1c9bb3979b914bcdaae0143eefc53ec8e9e8b8dc
Matias Barandiaran <115642529+m4mbo@users.noreply.github.com>
2024-01-28 12:32:59 +0100

    Final  change

--- assets/Shaders/Blink.glsl
+++ assets/Shaders/Blink.glsl
@@ -10,7 +10,6 @@ uniform float u_time;
 void main() {
     vec2 uv = v_texCoords;
     vec4 col = texture2D(u_texture, v_texCoords) * v_color;
-
     float a = abs(sin(u_time));
     vec3 temp = col.rgb + a * 0.4;
     gl_FragColor = vec4(temp, col.a);
